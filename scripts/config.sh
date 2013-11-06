f626578472f07e99a9f63ea571157d0829b7da5b
Gabe Carleton-Barnes <thegcb@gmail.com>
2013-11-05 21:23:00 -0800

    Config for Pantheon deploy.

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,8 +4,8 @@
 PROJECT=ghg
 
 # pantheon, acquia, generic
-HOSTTYPE=
+HOSTTYPE=pantheon
 
 SITENAME="Gamers Helping Gamers"
 
-GITREPO=
+GITREPO="ssh://codeserver.dev.e31cc06c-782b-4b2e-a3fb-16585c8f4caa@codeserver.dev.e31cc06c-782b-4b2e-a3fb-16585c8f4caa.drush.in:2222/~/repository.git"
